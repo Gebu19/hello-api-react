22a2e0532a63b23f39894062c161900b292ecc1c
Gebu19 <gebu830@gmail.com>
2026-01-30 15:44:00 +0700

    js 4 assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import TestAPI from './components/testApi'
+import Item from './components/item'
+import { Items } from './components/item1'
+import { ItemDetail } from './components/itemDetail'
 import './App.css'
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
   return (
     <Routes>
       <Route path='/test_api' element={<TestAPI />} />
+      <Route path='/items' element={<Items />} />
+      <Route path='/items/:id' element={<ItemDetail />} />
+
     </Routes>
   )
 }
