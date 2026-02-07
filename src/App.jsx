ef1d00b1c5640fdef22ef9260c725e3c79d4ba5b
Gebu19 <gebu830@gmail.com>
2026-02-07 18:01:57 +0700

    nextjs 5 done

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,32 @@ import Item from './components/item'
 import { Items } from './components/item1'
 import { ItemDetail } from './components/itemDetail'
 import './App.css'
+import RequireAuth from './middleware/RequireAuth'
+import Profile from './components/Profile'
+import Login from './components/Login'
+import Logout from './components/Logout'
+import UserList from './components/UserList'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <Routes>
       <Route path='/test_api' element={<TestAPI />} />
       <Route path='/items' element={<Items />} />
       <Route path='/items/:id' element={<ItemDetail />} />
-
+      <Route path='/login' element={<Login />} />
+      <Route path='/profile' element={
+        <RequireAuth>
+          <Profile />
+        </RequireAuth>
+      } />
+      <Route path='/logout' element={
+        <RequireAuth>
+          <Logout />
+        </RequireAuth>
+      } />
+      <Route path='/users' element={<UserList />} />
     </Routes>
   )
 }
